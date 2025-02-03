bcc5d739e4d1ce6b8790813b23c2e8f9c13782a6
Carlos Ariza <carlosariza7459@gmail.com>
2025-02-03 15:05:05 -0300

    update projects

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -11,7 +11,6 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
 };
 
 const style = {
